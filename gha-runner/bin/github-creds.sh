d19dd37d56f3e99a6234fcd8b905c474a49925ff
Paul Carlton <paul.carlton414@gmail.com>
2025-01-24 13:49:52 +0000

    update runner image

--- gha-runner/bin/github-creds.sh
+++ gha-runner/bin/github-creds.sh
@@ -42,11 +42,11 @@ if [ -n "$debug" ]; then
     echo "Current directory: $(pwd)"
 fi
 
-export ${type}_app_install_id="$(kubectl get secret -n ${POD_NAMESPACE} github-${type}-auth -o=jsonpath='{.data.github_app_installation_id}' | base64 -d)"
-export ${type}_app_client_id="$(kubectl get secret -n ${POD_NAMESPACE} github-${type}-auth -o=jsonpath='{.data.github_app_client_id}' | base64 -d)"
+export app_install_id="$(kubectl get secret -n ${POD_NAMESPACE} github-${type}-auth -o=jsonpath='{.data.github_app_installation_id}' | base64 -d)"
+export app_id="$(kubectl get secret -n ${POD_NAMESPACE} github-${type}-auth -o=jsonpath='{.data.github_app_id}' | base64 -d)"
 kubectl get secret -n ${POD_NAMESPACE} github-${type}-auth -o=jsonpath='{.data.github_app_private_key}' | base64 -d | awk '{gsub(/\\n/,"\n")}1' > /tmp/${type}-key.pem
-JWT=$(/home/runner/bin/app-jwt.py /tmp/${type}-key.pem $${type}_app_client_id | cut -f2 -d: | sed s/\ //g)
-curl -s --request POST --url "https://api.github.com/app/installations/$${type}_app_install_id/access_tokens" \
+JWT=$(/home/runner/bin/app-jwt.py /tmp/${type}-key.pem $app_id | cut -f2 -d: | sed s/\ //g)
+curl -s --request POST --url "https://api.github.com/app/installations/app_install_id/access_tokens" \
      --header "Accept: application/vnd.github+json" --header "Authorization: Bearer $JWT" --header "X-GitHub-Api-Version: 2022-11-28" > /tmp/${type}-token.json
 export GHT="$(jq -r '.token' /tmp/${type}-token.json)"
 export GHT_EXPIRY="$(jq -r '.expires_at' /tmp/${type}-token.json)"
