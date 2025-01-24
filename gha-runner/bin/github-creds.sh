7542ff814979ce23a49894b543fb0ee40132b9ba
Paul Carlton <paul.carlton414@gmail.com>
2025-01-24 13:58:23 +0000

    update runner image

--- gha-runner/bin/github-creds.sh
+++ gha-runner/bin/github-creds.sh
@@ -19,7 +19,7 @@ function args() {
   arg_index=0
   while (( arg_index < arg_count )); do
     case "${arg_list[${arg_index}]}" in
-          "--debug") debug="--debug";set -x;;
+          "--debug") debug="--debug"; debug_curl="-v";set -x;;
           "--type") (( arg_index+=1 ));type=${arg_list[${arg_index}]};;
                "-h") usage; exit;;
            "--help") usage; exit;;
@@ -46,8 +46,16 @@ export app_install_id="$(kubectl get secret -n ${POD_NAMESPACE} github-${type}-a
 export app_id="$(kubectl get secret -n ${POD_NAMESPACE} github-${type}-auth -o=jsonpath='{.data.github_app_id}' | base64 -d)"
 kubectl get secret -n ${POD_NAMESPACE} github-${type}-auth -o=jsonpath='{.data.github_app_private_key}' | base64 -d | awk '{gsub(/\\n/,"\n")}1' > /tmp/${type}-key.pem
 JWT=$(/home/runner/bin/app-jwt.py /tmp/${type}-key.pem $app_id | cut -f2 -d: | sed s/\ //g)
-curl -s --request POST --url "https://api.github.com/app/installations/app_install_id/access_tokens" \
+if [ -n "$debug" ]; then
+    echo "JWT: $JWT"
+    echo "JWT length: $(echo $JWT | wc -c)"
+fi
+curl ${debug_curl:--s} --request POST --url "https://api.github.com/app/installations/app_install_id/access_tokens" \
      --header "Accept: application/vnd.github+json" --header "Authorization: Bearer $JWT" --header "X-GitHub-Api-Version: 2022-11-28" > /tmp/${type}-token.json
+if [ -n "$debug" ]; then
+    echo "token data"
+    cat /tmp/${type}-token.json
+fi
 export GHT="$(jq -r '.token' /tmp/${type}-token.json)"
 export GHT_EXPIRY="$(jq -r '.expires_at' /tmp/${type}-token.json)"
 echo "GITHUB_TOKEN=$GHT" >> "$GITHUB_ENV"
